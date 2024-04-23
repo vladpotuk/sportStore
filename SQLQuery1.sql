3bcc54bd11e1cb1fd96357f5a598008fff19bfd3
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-23 19:56:15 +0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+USE SportStore;
+GO
+
+-- Delete all data from the Products table
+DELETE FROM Products;
+GO
+
+-- Delete all data from the Employees table
+DELETE FROM Employees;
+GO
+
+-- Delete all data from the Customers table
+DELETE FROM Customers;
+GO
+
+-- Delete all data from the Sales table
+DELETE FROM Sales;
+GO
